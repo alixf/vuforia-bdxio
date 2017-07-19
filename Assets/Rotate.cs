25eb0b5606d62f843a40e8d8878617c4e09f3e74
Alix <alix.fumoleau@gmail.com>
2017-07-19 09:14:42 +0200

    Add starting code

---
+++ Assets/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	void Update () {
+        transform.Rotate(0f, 180f * Time.deltaTime, 0f, Space.World);
+	}
+}
